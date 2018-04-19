6a2d4353aeee79e80635580d179cffd866abb1de
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 19:48:05 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,11 +28,10 @@ def adding
                   "oil paintings" => 3
                  }
                  
-shipping_manifest  = {"muskets" => "2"}
-shipping_manifest ["muskets"] = "2"
+shipping_manifest["muskets"] = 2
 
-shipping_manifest  = {"gun powder" => "4"}
-shipping_manifest["gun powder"] = "4"
+shipping_manifest["gun powder"] = 4
+
 
 shipping_manifest 
 
