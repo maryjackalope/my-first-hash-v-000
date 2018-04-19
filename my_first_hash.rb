19c3db0b1d072f5ac0a8b51e8486b53b24998a10
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 19:40:15 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,12 @@ end
 
 
 def shipping_manifest
-shipping_manifest ={ "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+shipping_manifest ={
+    "whale bone corsets" => 5
+    "porcelain vases" => 2, 
+    "oil paintings" => 3
+  
+}
 end
 
 def retrieval
@@ -13,6 +18,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  shipping_manifest["oil paintings"]
 end
 
 def adding
