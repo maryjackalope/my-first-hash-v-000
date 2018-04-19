1ec63723ebaad92899a4c8205eedbef1dc70711c
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 19:42:07 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,7 +27,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
+shipping_manifest["key2"] = "value2"
 shipping_manifest 
 
 end
